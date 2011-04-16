71f21bf91d8b92a39f348cc13d3d013f4c03ea96
Adam Stacoviak <adam@stacoviak.com>
2011-04-15 19:43:04 -0500

    Cleaned up app.rb, set haml format type to HTML5 and updated cruft in the two layouts

--- app.rb
+++ app.rb
@@ -1,32 +1,32 @@
-# Load before Sinatra
 require 'rubygems'
-require 'compass' # must be loaded before sinatra
-
-# Load Sinatra
 require 'sinatra'
+
+# Require partial helper
 require 'lib/render_partial'
 
-# Load after Sinatra
-require 'haml' # must be loaded after sinatra
+# Haml and Compass
+require 'haml'
+require 'compass'
 
-# Set Sinatra's variables
+# Set Sinatra variables
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, 'views'
 set :public, 'public'
+set :haml, {:format => :html5} # default Haml format is :xhtml
 
 # Configure Compass
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config.rb'))
 end
 
-# At a minimum the main sass file must reside within the views directory
-# We create /views/stylesheets where all our sass files can safely reside
+# Render stylesheets
 get '/stylesheets/:file.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass(:"stylesheets/#{params[:file]}", Compass.sass_engine_options)
 end
 
+# Application routes
 get '/about' do
   haml :about, :layout => :'layouts/page'
 end
