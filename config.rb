1a8f3443b8357e1b7bc296fd46b0bc06f7b7b50c
Adam Stacoviak <adam@stacoviak.com>
2011-04-16 22:44:01 -0500

    Updated the gems to include grid-coordinates and meyer-reset, updated the Compass config

--- config.rb
+++ config.rb
@@ -5,14 +5,32 @@ if defined?(Sinatra)
 else
   # this is the configuration to use when running within the compass command line tool.
   css_dir = File.join 'public', 'stylesheets'
+  # Determine whether Compass asset helper functions generate relative
+  # or absolute paths
   relative_assets = true
   environment = :production
 end
- 
-# This is common configuration
-sass_dir = File.join 'views', 'stylesheets'
-images_dir = File.join 'public', 'images'
-http_path = "/"
-http_images_path = "/images"
-http_stylesheets_path = "/stylesheets"
-output_style = :expanded #compressed #nested #compact
+
+# Compass Configuration
+
+require 'grid-coordinates'
+require 'meyer-reset'
+
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/stylesheets'
+http_images_path      = '/images'
+http_javascripts_path = '/javascripts'
+
+# File system locations
+sass_dir              = File.join 'views', 'stylesheets'
+images_dir            = File.join 'public', 'images'
+javascripts_dir       = File.join 'public', 'javascripts'
+
+# Set to true for easier debugging
+line_comments         = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :expanded
+
+# Learn more: http://compass-style.org/docs/tutorials/configuration-reference/
